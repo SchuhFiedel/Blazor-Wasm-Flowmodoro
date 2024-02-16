d6e46721cdc93cc2ed812d0d97b5555c8da049ce
Maximiliano Zulli <if19b183@dev.azure.com>
2024-02-16 20:57:31 +0100

    TaskList -> Simple Drag and Drop reordering of tasks works

---
+++ Client/Models/JobModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Flowmodoro.Client.Models
+{
+    public class JobModel
+    {
+        public Guid Id  {  get;  set; }
+        public JobStatuses Status {  get; set; }
+        public string Description  {  get;  set; }
+        public DateTime LastUpdated  { get; set; }
+    }
+    public enum JobStatuses
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
+
