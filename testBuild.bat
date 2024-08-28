7210e22ee7ff09f6a336c1d6a5f7b76b24d046d7
Maximiliano Zulli <maximiliano.zulli@gmail.com>
2024-08-28 12:44:01 +0200

    refactoring of test build files - build logs on test environment now limited to 1KB - rename of remoteMachineCommand.txt to testRemoteMachineCommand.txt for better understanding of environment it is used in

--- testBuild.bat
+++ testBuild.bat
@@ -7,5 +7,5 @@ echo "2/4 --- Copy to test environment"
 pscp -P 22 -pw %1 "D:\Documents\_Mein Stuff\ProgrammingStuff\rando\Flowmodoro\Server\bin\flowmodoro.tar" pi@192.168.0.122:/home/pi/Images
 
 echo "ssh to test env and install"
-START /WAIT putty.exe pi@192.168.0.122:22 -pw %1 -m ./remoteMachineCommands.txt -t
+START /WAIT putty.exe pi@192.168.0.122:22 -pw %1 -m ./testRemoteMachineCommands.txt -t
 echo "Done!"
