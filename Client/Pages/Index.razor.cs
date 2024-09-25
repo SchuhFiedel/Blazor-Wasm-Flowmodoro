7122499149870bb48de8969579a0e26ec6970035
Maximiliano Zulli <maximiliano.zulli@gmail.com>
2024-09-25 17:04:20 +0200

    #25 Tasks are being saved and loaded on page close, and every 30 seconds after the page is opened - also the Project has been upgraded to .net7, also the docker file #27

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,29 +1,90 @@
 ﻿using Flowmodoro.Client.Interfaces;
 using Flowmodoro.Client.Models;
+using Flowmodoro.Client.Shared;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Recurop;
 
 namespace Flowmodoro.Client.Pages
 {
-    public partial class Index
+    public partial class Index: IDisposable
     {
- 
+        [Inject]
+        protected Blazored.LocalStorage.ISyncLocalStorageService syncLocalStorageService { get; set; } = null!;
 
         [CascadingParameter(Name = "ErrorComponent")]
         protected IToastComponent ToastComponent { get; set; } = null!;
+        [Inject]
+        public RecurringOperationsManager Recurop { get; init; } = null!;
 
-        void LogError(Exception ex)
+        private static Action callNonStaticFunction = null!;
+        private DragAndDropListComponent DragDropListCompRef { get; set; } = null!;
+        private bool onRefPopulated = false;
+
+        public JobModel? SelectedJob = null;
+        public List<JobModel> JobModelList = new List<JobModel>();
+        public RecurringOperation SaveInIntervalsOperation { get; private set; } = null!;
+
+        private void LogError(Exception ex)
         {
             this.ToastComponent.ShowError(ex.Message, ex.InnerException?.Message ?? "");
         }
 
-        public JobModel? SelectedJob = null;
-
-        public List<JobModel> JobModelList = new List<JobModel>();
-
-        void OnJobItemSelected(Guid? itemId)
+        private void OnJobItemSelected(Guid? itemId)
         {
             JobModel? item = JobModelList.Find(x => x.Id == itemId);
             this.SelectedJob = item;
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if(onRefPopulated == false && DragDropListCompRef != null)
+            {
+                onRefPopulated = true;
+                var selectedJobId = syncLocalStorageService.GetItem<Guid?>(nameof(SelectedJob));
+                if (selectedJobId.HasValue)
+                {
+                    OnJobItemSelected(selectedJobId.Value);
+                    Console.WriteLine("ref", DragDropListCompRef.JobsList.Count());
+                    DragDropListCompRef.TrackingClickHandler(selectedJobId.Value);
+
+                }
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            Console.WriteLine(nameof(OnAfterRender));
+            callNonStaticFunction = OnReload;
+            var list = syncLocalStorageService.GetItem<List<JobModel>>(nameof(JobModelList));
+            if (list != null && list.Any())
+            {
+                this.JobModelList = list;
+            }
+
+            SaveInIntervalsOperation = new("SaveInIntervals");
+            SaveInIntervalsOperation.Operation = OnReload;
+            SaveInIntervalsOperation.OperationFaulted += LogError;
+
+            Recurop.StartRecurring(SaveInIntervalsOperation, TimeSpan.FromSeconds(30));
+            
+        }
+
+        [JSInvokable]
+        public static void PageAboutToBeReloaded()
+        {
+            callNonStaticFunction.Invoke();
+        }
+
+        public void OnReload()
+        {
+            syncLocalStorageService.SetItem(nameof(JobModelList), JobModelList);
+            syncLocalStorageService.SetItem(nameof(SelectedJob), SelectedJob?.Id);
+        }
+
+        public void Dispose()
+        {
+            Recurop.CancelRecurring(SaveInIntervalsOperation);
         }
     }
 
